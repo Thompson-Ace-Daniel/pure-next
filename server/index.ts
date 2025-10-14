caeb78e87295437b367c28db969520c4d725cad5
Thompson Daniel <thompsonacedaniel@gmail.com>
2025-10-14 14:53:22 +0100

    initial commit

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -63,7 +65,7 @@ app.use((req, res, next) => {
   const port = parseInt(process.env.PORT || '5000', 10);
   server.listen({
     port,
-    host: "0.0.0.0",
+    host: "127.0.0.1", // <-- CHANGED from "0.0.0.0" to "127.0.0.1" to fix ENOTSUP error
     reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
