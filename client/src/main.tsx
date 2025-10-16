acc66eb0037b7106497773b2557595c68c885663
Thompson Daniel <thompsonacedaniel@gmail.com>
2025-10-16 13:23:48 +0100

    removed vercel speed insights

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
-    <SpeedInsights />
   </>
 );
