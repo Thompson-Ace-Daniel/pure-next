8bbe3e98a00a9721d14e5c66d5ebbc41f467c290
Thompson Daniel <thompsonacedaniel@gmail.com>
2025-10-23 14:30:14 +0100

    vercel --prod

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,17 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { inject } from "@vercel/analytics";
 
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-  </>
+// Initialize analytics (must be called before render)
+inject();
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Root element not found. Make sure <div id='root'></div> exists in index.html.");
+}
+
+createRoot(container).render(
+  <App />
 );
